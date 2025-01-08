771e1b96bacf0a67feaa22a05c9b8413829c93b6
jinhyeok <wlsgur348@gmail.com>
2025-01-08 14:53:07 +0900

    [FEAT] 필요한 View Activity 추가

--- app/src/main/java/com/mindJellyProject/mindjelly/MainActivity.java
+++ app/src/main/java/com/mindJellyProject/mindjelly/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mindJellyProject.mindjelly;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,47 @@ public class MainActivity extends AppCompatActivity {
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // 버튼 초기화
+        Button btnTodayJelly = findViewById(R.id.btnTodayJelly);
+        Button btnJellyDrawer = findViewById(R.id.btnJellyDrawer);
+        Button btnAgingRoom = findViewById(R.id.btnAgingRoom);
+        Button btnJellyMuseum = findViewById(R.id.btnJellyMuseum);
+        Button btnSelectionBox = findViewById(R.id.btnSelectionBox);
+
+        btnTodayJelly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnJellyDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnAgingRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnJellyMuseum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSelectionBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
