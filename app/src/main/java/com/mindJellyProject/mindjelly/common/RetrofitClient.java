09603b6a87325e03ac018d0206b4fe39fe88dfa8
KTDS <wlsgur348@gmail.com>
2026-05-12 16:03:44 +0900

    feat(01-B): wire retrofit through auth interceptor
    - Build RetrofitClient as a context-based singleton backed by OkHttpClient
    - Add reset support and preserve compile until repositories migrate to context APIs

--- app/src/main/java/com/mindJellyProject/mindjelly/common/RetrofitClient.java
+++ app/src/main/java/com/mindJellyProject/mindjelly/common/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.mindJellyProject.mindjelly.common;
 
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,31 +10,58 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * @author : Jinhyeok
  * @className : com.mindJellyProject.mindjelly.common
  * @description : API 관리하는 공용 Retrofit Client
- * @modification : 2025-01-03(Jinhyeok) 수정
+ * @modification : 2026-05-12(KTDS) OkHttp AuthInterceptor 적용
  * @date : 2025-01-03
 
  * ====개정이력(Modification Information)====
  * 수정일        수정자        수정내용
  * -----------------------------------------
  * 2025-01-03     Jinhyeok        주석 생성
+ * 2026-05-12     KTDS            JWT Bearer 토큰 주입 인터셉터 연결
  */
 public class RetrofitClient {
-    private static Retrofit retrofit;
     private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static RetrofitClient instance;
+    private final Retrofit retrofit;
 
-    // Retrofit 인스턴스 생성
-    public static Retrofit getInstance() {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+    private RetrofitClient(Context context) {
+        Context appContext = context.getApplicationContext();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new AuthInterceptor(appContext))
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    // RetrofitClient 인스턴스 생성
+    public static synchronized RetrofitClient getInstance(Context context) {
+        if (instance == null) {
+            instance = new RetrofitClient(context);
         }
-        return retrofit;
+        return instance;
     }
 
     // API 인터페이스 제공 메서드
+    public static <T> T createService(Class<T> serviceClass, Context context) {
+        return getInstance(context).retrofit.create(serviceClass);
+    }
+
+    // 기존 Repository들이 Context 주입으로 마이그레이션되기 전까지 컴파일을 유지하는 임시 경로
+    @Deprecated
     public static <T> T createService(Class<T> serviceClass) {
-        return getInstance().create(serviceClass);
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(serviceClass);
+    }
+
+    // 로그아웃이나 토큰 갱신 시 싱글턴 재초기화
+    public static synchronized void reset() {
+        instance = null;
     }
 }
